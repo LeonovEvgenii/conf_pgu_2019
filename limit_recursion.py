ed142a36543f93810e48a859098e9055ad8d5b97
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2019-03-30 19:32:22 +0500

    dont work

--- limit_recursion.py
+++ limit_recursion.py
@@ -1,17 +1,36 @@
 from pyDatalog import pyDatalog
-pyDatalog.create_terms('factorial, N, X')
 
-import logging
-from pyDatalog import pyEngine
-pyEngine.Logging = True
-logging.basicConfig(level=logging.INFO)
+pyDatalog.create_terms('factorial, N, X, Y, Z, M, fff')
 
-factorial[N, X] = X+factorial[N-1 ,X-1]
-factorial[1, X] = X
 
-print(factorial[3, 10]==X)
+fff(Z, M) <= (Z==M*2)
+factorial(3, X) <= (X==3+Z) & fff(Z, 2)
 
-# factorial[N] = N*factorial[N-1]
-# factorial[1] = 1
+print(factorial(3, X))
 
-# print(factorial[3]==N)
+
+
+
+
+factorial(3, 4)
+factorial(3, X) <= (X==3+3)
+
+print(factorial(3, X))
+
+
+
+
+
+#################
+# factorial(N, Y, X) <= (X==Y*2) & (N>0)
+
+# print(factorial(1, 10, X))
+
+
+
+
+
+# + factorial(1, 1)
+# factorial(N, X) <= (X==N+Z) & (factorial(N-1, Z)) & (N>1)
+
+# print(factorial(3, X))

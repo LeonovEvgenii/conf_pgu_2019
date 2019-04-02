9ab124d4fa58a45e171ae83ce44cd7167bcd45fc
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2019-04-02 21:06:02 +0500

    sucsessful

--- mixin.py
+++ mixin.py
@@ -1,28 +1,31 @@
-from pyDatalog import pyDatalog 
+from pyDatalog import pyDatalog
+import sys
 
-pyDatalog.create_terms('X, Y, Z')
+pyDatalog.create_terms('X, Y, Z, N')
 
 class Associate(pyDatalog.Mixin):
     
-    def __init__(self, left, right): 
+    def __init__(self, name, bind): 
         super(Associate, self).__init__()
-        self.left = left
-        self.right = right     
+        self.name = name
+        self.bind = bind     
     
     def __repr__(self):
-        return self.left+' -> '+self.right
+        return self.name
 
-q_w = Associate('q', 'w')
-w_e = Associate('w', 'e')
-e_r = Associate('e', 'r')
-r_t = Associate('r', 't')
-t_y = Associate('t', 'y')
+q = Associate('q', None)
+w = Associate('w', q)
+e = Associate('e', w)
+# r = Associate('r', e)
+# t = Associate('t', r)
+# y = Associate('y', t)
 
 
-Associate.indirect_manager(X,Y) <= (Associate.left[X]==Y) & (Y != None)
-# Associate.indirect_manager(X,Y) <= (Associate.left[X]==Z) & Associate.indirect_manager(Z,Y) & (Y != None)
 
-print(Associate.indirect_manager(X,Y))
+Associate.indirect_manager(X,Y, N) <= (Associate.bind[X]==Y) & (Y != None) & (N>0)
+Associate.indirect_manager(X,Y, N) <= (Associate.bind[X]==Z) & Associate.indirect_manager(Z,Y, N-1) & (Y != None) & (N>0)
+
+print(Associate.indirect_manager(X,Y, int(sys.argv[1])))
 
 
 # Employee.indirect_manager(X,Y) <= (Employee.manager[X]==Y) & (Y != None)
@@ -30,31 +33,3 @@ print(Associate.indirect_manager(X,Y))
 
 
 # print(Employee.indirect_manager(X,Y))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print(Associate.left[X] == 'q')

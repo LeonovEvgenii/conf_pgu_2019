cfee2c17bda61886c068f6ff1448f24498258521
LeonovEvgenii <leonovevgenii2015@yandex.ru>
2019-04-13 19:15:22 +0500

    stop_words

--- conference.py
+++ conference.py
@@ -5,18 +5,22 @@ import sys, random, os
 from get_definition import get_definition
 from get_keywords import get_keywords, filter_keywords
 from write_to_file import write_to_file
+from stop_words import is_stop_word
 
 
 def get_graph(definition = "Дерево", n = 2):
 	if n > 0:
+		definition = definition.lower()
+
 		definition_text = get_definition(definition)
 		
 		pairs = []
 
 		for keyword in filter_keywords(get_keywords(definition_text)):
-			yield [definition, keyword]
-			for x in get_graph(keyword, n - 1):
-				yield x
+			if keyword != definition and not is_stop_word(keyword):
+				yield [definition, keyword]
+				for x in get_graph(keyword, n - 1):
+					yield x
 			
 if __name__ == '__main__':
 
